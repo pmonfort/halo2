c5653669d9dd4af22d859eacbb21dc545e1e99a3
Amir Sharif <budgeneration@gmail.com>
2025-05-30 08:39:44 -0700

    Update Ruby version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_04_27_195111) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_27_195111) do
   create_table "people", force: :cascade do |t|
     t.string "openalex_id"
     t.string "first_name", null: false
